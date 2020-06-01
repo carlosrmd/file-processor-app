bccf5c97867a1bb2dfc8ca888bb1aead4cd5d41d
Carlos Martínez <Carlos.Martinez@corcoran.com>
2020-06-01 16:34:35 -0300

    project dockerization

---
+++ docker/database/sql-scripts/CreateTable.sql
@@ -0,0 +1,10 @@
+create table Item(
+	site VARCHAR(255),
+	id VARCHAR(255),
+	price DECIMAL(13,2),
+	name VARCHAR(255),
+	description VARCHAR(255),
+	nickname VARCHAR(255),
+	start_time DATE,
+	primary key(id)
+);
